e6cb701e5b76c612646b951338d611d378b00331
Michael Eisenbraun <eisenbm@algonquincollege.com>
2024-07-29 17:20:10 -0400

    Creates final version

---
+++ Data/DataService.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace TaskManager;
+
+public class DataService (string connectionString)
+{
+  private readonly string connectionString = connectionString;
+
+  public async Task<List<T>> LoadData<T, U> (string sql, U parameters)
+  {
+    using (var connection = new MySqlConnection(connectionString))
+    {
+      var rows = await connection.QueryAsync<T>(sql, parameters);
+      return rows.ToList();
+    }
+  }
+
+  public async Task SaveData<T>(string sql, T parameters)
+  {
+    using (var connection = new MySqlConnection(connectionString))
+    {
+      await connection.ExecuteAsync(sql, parameters);
+    }
+  }
+}
